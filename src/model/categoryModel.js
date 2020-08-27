d4294890ffa1a9f505d9e1198664ac29607a7149
nova zaky <novazaky1@gmail.com>
2020-08-27 10:21:18 +0700

    update categoryModel.js

--- src/model/categoryModel.js
+++ src/model/categoryModel.js
@@ -4,6 +4,17 @@ import sch from '../schema/category';
 class categoryModel extends Models{
     constructor(){
         super(sch)
+        this.level=10
+    }
+
+    async insert(obj){
+        if(this.udata.payload.level<this.level)throw Error('Anda tidak punya akses untuk menambah data ini!')
+        return await this.model.create(this.doConvertParam(obj))
+    }
+
+    doConvertParam(body){
+        body.createdBy = this.udata.payload.id
+        return body
     }
 
 }
