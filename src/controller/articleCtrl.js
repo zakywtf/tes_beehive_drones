c4e4499f2dc7d405b4db95ff0e96635da481eeab
nova zaky <novazaky1@gmail.com>
2020-08-29 22:44:20 +0700

    add filter tags

--- src/controller/articleCtrl.js
+++ src/controller/articleCtrl.js
@@ -10,10 +10,20 @@ rtr.get('/pagination/:page/:perPage',async(req,res)=>{
     handleRequest(req, res, async (body)=>{
         const udata = res.locals.udata.payload
         const {page, perPage}=req.params;
+        const {tags}=req.query
+        const qry = {
+            deleted:0,
+            author:mg.Types.ObjectId(udata.id),
+            tags:{$elemMatch:{category_id:mg.Types.ObjectId(tags)}}
+        }
         if(udata.level>5){
             return await model.paging(perPage, (((page-1) * perPage)), {deleted:0}, {createdAt:-1});
         }else{
-            return await model.paging(perPage, (((page-1) * perPage)), {deleted:0, author:mg.Types.ObjectId(udata.id)}, {createdAt:-1});
+            if(tags){
+                return await model.paging(perPage, (((page-1) * perPage)), qry, {createdAt:-1});
+            }else{
+                return await model.paging(perPage, (((page-1) * perPage)), {deleted:0, author:mg.Types.ObjectId(udata.id)}, {createdAt:-1});
+            }
         }
     })
 })
